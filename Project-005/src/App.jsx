831965f8dc6ff1960e59738978f44f2813d956aa
gilman qazi <gilmanqazi@gmail.com>
2025-12-26 15:49:22 +0530

    project-005

---
+++ Project-005/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-[100vh] w-[100%] bg-black flex justify-center items-center '>
+<div className='h-[50vh] w-[20vw] bg-sky-900 rounded-3xl'>
+ <h1 className='text-center py-5 text-xl font-semibold'>Gilman Quazi</h1>
+ <p  className='text-center py-2 text-s font-semibold'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati harum, itaque maxime voluptatibus officiis voluptate ipsum officia facilis sequi at ea omnis voluptatum? Veniam, voluptas? Similique eligendi iure deserunt aut voluptate quasi, fuga suscipit repudiandae fugiat quam beatae dolor nemo autem quia ratione assumenda atque omnis libero. Doloribus, obcaecati ut.</p>
+</div>
+    </div>
+  );
+};
+
+export default App;
